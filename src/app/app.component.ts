90526df90749c2ac2d358cb13692311e89077ab6
Joan Vasquez <yoloprogramo22@gmail.com>
2019-06-06 10:53:38 -0800

    Pagination Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pagination';
+  private todoList: object[] = [];
+  private maxSizePagination: number = 5;
+  private autoHidePagination: boolean = true;
+
+  private paginationConfig: PaginationInstance = {
+    id: 'advanced',
+    itemsPerPage: 6,
+    currentPage: 1
+  };
+
+  private labels: object = {
+    previousLabel: 'Back',
+    nextLabel: 'Next',
+    screenReaderPaginationLabel: 'Pagination',
+    screenReaderPageLabel: 'page',
+    screenReaderCurrentLabel: `You're on page`
+  };
+
+  private onPageChange(number: number) {
+    this.paginationConfig.currentPage = number;
+  }
+
+  ngOnInit() {
+    for (let i = 0; i <= 30; i++) {
+      this.todoList.push({
+        id: i,
+        title: `Home Work ${i}`,
+        completed: false
+      });
+    }
+  }
 }
